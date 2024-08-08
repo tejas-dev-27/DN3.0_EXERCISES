abb3a7ee54bd12ddfc07c0f187ed031f2d859671
TEJASWIN D <96116128+tejas-dev-27@users.noreply.github.com>
2024-08-08 15:41:16 +0530

    Delete Upload1/Week_2_Exercises directory

--- Upload1/Week_2_Exercises/LibraryManagement/src/main/java/com/example/librarymanagement/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.librarymanagement.repository;
-
-import com.example.librarymanagement.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
