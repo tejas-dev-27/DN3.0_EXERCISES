b25ef88409c5dafa9855fca87edc90ebb385e987
TEJASWIN D <96116128+tejas-dev-27@users.noreply.github.com>
2024-08-22 22:44:54 +0530

    fourth commit

---
+++ Week_4_Exercises/BookstoreAPI/src/main/java/com/example/bookstoreapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.bookstoreapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Handle BookNotFoundException
+    @ExceptionHandler(BookNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorResponse handleBookNotFoundException(BookNotFoundException ex) {
+        return new ErrorResponse("BOOK_NOT_FOUND", ex.getMessage());
+    }
+
+    // Handle generic exceptions
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorResponse handleGenericException(Exception ex) {
+        return new ErrorResponse("INTERNAL_SERVER_ERROR", "An unexpected error occurred");
+    }
+}
---
+++ Week_4_Exercises/BookstoreAPI/src/main/java/com/example/bookstoreapi/mapper/BookMapper.java
@@ -0,0 +1,16 @@
+package com.example.bookstoreapi.mapper;
+
+import com.example.bookstoreapi.dto.BookDTO;
+import com.example.bookstoreapi.model.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookMapper {
+
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    BookDTO bookToBookDTO(Book book);
+
+    Book bookDTOToBook(BookDTO bookDTO);
+}
